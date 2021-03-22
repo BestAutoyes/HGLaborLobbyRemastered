4d68cea64f7c40839d6356cd0da22d0f90234a39
copyandexecute <58366920+copyandexecute@users.noreply.github.com>
2021-03-22 19:05:29 +0100

    Update HGInformationListenerV2.kt

--- src/main/kotlin/net/axay/hglaborlobby/hgqueue/HGInformationListenerV2.kt
+++ src/main/kotlin/net/axay/hglaborlobby/hgqueue/HGInformationListenerV2.kt
@@ -2,11 +2,13 @@ package net.axay.hglaborlobby.hgqueue
 
 import de.hglabor.utils.noriskutils.jedis.JChannels
 import de.hglabor.utils.noriskutils.queue.hg.HGGameInfo
+import net.axay.hglaborlobby.gui.guis.HGQueueGUI
 import net.axay.hglaborlobby.main.gson
 import net.axay.kspigot.gui.ForInventoryThreeByNine
 import net.axay.kspigot.gui.elements.GUICompoundElement
 import org.bukkit.inventory.ItemStack
 import redis.clients.jedis.JedisPubSub
+import java.nio.charset.StandardCharsets
 
 object HGInformationListenerV2 : JedisPubSub() {
     override fun onMessage(channel: String?, message: String?) {
@@ -17,6 +19,15 @@ object HGInformationListenerV2 : JedisPubSub() {
                 val compoundElements = mutableListOf<GUICompoundElement<ForInventoryThreeByNine>>()
 
                 //TODO nur das eine halt updaten lel
+                hgInfos.sortedWith(compareBy<HGInfo> { it.gameState }.thenByDescending { it.onlinePlayers }.thenBy { it.serverName })
+                        .forEach { hgInfo ->
+                            hgInfo.item = hgInfo.getNewItem()
+                            items.add(hgInfo.item!!)
+                            HGInfo.infos[hgInfo.serverName!!] = hgInfo
+                        }
+                items.forEach { compoundElements.add(HGQueueGUI.HGQueueGUICompoundElement(it)) }
+                HGQueueGUI.setContent(compoundElements)
+
                 println(hgInfos.toString())
             }
         }
